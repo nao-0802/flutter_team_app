692a4a399d66b8df72ff2f35fde6aad49cddf104
nao-0802 <tky2302123@stu.o-hara.ac.jp>
2025-11-20 17:01:39 +0900

    修正

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.flutter_team_app"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.flutter_team_app"
         minSdk = flutter.minSdkVersion
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
